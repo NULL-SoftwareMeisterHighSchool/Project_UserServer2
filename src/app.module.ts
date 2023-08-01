0618d9f377282665039f17d8eaf86a56c24b9a15
onee-only <kimww0306@gmail.com>
2023-08-01 19:14:58 +0900

    typeorm dependency 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { ArticleModule } from './domain/article/article.module';
 import { AuthModule } from './domain/auth/auth.module';
 import { CommentModule } from './domain/comment/comment.module';
 import { UserModule } from './domain/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMConfig } from './global/config';
 
 @Module({
-  imports: [ArticleModule, AuthModule, CommentModule, UserModule],
+  imports: [
+    ArticleModule,
+    AuthModule,
+    CommentModule,
+    UserModule,
+    TypeOrmModule.forRoot(TypeORMConfig),
+  ],
 })
 export class AppModule {}
---
+++ src/global/config/index.ts
@@ -0,0 +1,1 @@
+export { TypeORMConfig } from './typeorm.config';
