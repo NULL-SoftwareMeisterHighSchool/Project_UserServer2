928a0deeb820c6160fc63f80219d0dcc6910b3fb
onee-only <kimww0306@gmail.com>
2023-08-02 20:25:18 +0900

    ADD: login route 추가

--- src/domain/auth/auth.controller.ts
+++ src/domain/auth/auth.controller.ts
@@ -1,13 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { StudentSignupRequestDto } from './dto/request';
-import { StudentSignupService } from './services';
+import { LoginRequestDto, StudentSignupRequestDto } from './dto/request';
+import { LoginService, StudentSignupService } from './services';
+import { LoginResponseDto } from './dto/response';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly studentSignupService: StudentSignupService) {}
+  constructor(
+    private readonly studentSignupService: StudentSignupService,
+    private readonly loginService: LoginService,
+  ) {}
 
   @Post('signup/student')
   async signup(@Body() request: StudentSignupRequestDto): Promise<void> {
     return await this.studentSignupService.execute(request);
+  }
+
+  @Post('login')
+  async login(@Body() request: LoginRequestDto): Promise<LoginResponseDto> {
+    return await this.loginService.execute(request);
   }
 }
