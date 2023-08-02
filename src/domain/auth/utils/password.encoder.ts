9334ea1dbd891c4da87cfd29be58761de36bfd19
onee-only <kimww0306@gmail.com>
2023-08-02 18:15:21 +0900

    CHORE: PasswordEncoder -> PasswordManager

--- src/domain/auth/utils/password.encoder.ts
+++ src/domain/auth/utils/password.encoder.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class PasswordEncoder {
+export class PasswordManager {
   private readonly SALT_ROUNDS = 5;
 
   async encode(password: string): Promise<string> {
     return await bcrypt.hash(password, this.SALT_ROUNDS);
+  }
+
+  async compare(candiate: string, password: string): Promise<boolean> {
+    return await bcrypt.compare(candiate, password);
   }
 }
