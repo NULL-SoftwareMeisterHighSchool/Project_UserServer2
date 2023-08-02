0978c74201ba2bb8fdaa9cb30da060883c8a6799
onee-only <kimww0306@gmail.com>
2023-08-02 18:19:54 +0900

    ADD: auth module에 providers 추가

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { PasswordManager } from './utils';
+import { StudentSignupService } from './services';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AuthController],
-  providers: [],
+  providers: [
+    // services
+    StudentSignupService,
+
+    // utils
+    PasswordManager,
+  ],
 })
 export class AuthModule {}
