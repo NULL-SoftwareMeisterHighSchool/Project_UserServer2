43cc092b660cf6f05c12db786ffbd68b34c2aec9
onee-only <kimww0306@gmail.com>
2023-08-03 14:32:47 +0900

    ADD: mail service export 추가

--- src/global/modules/mail/mail.module.ts
+++ src/global/modules/mail/mail.module.ts
@@ -1,9 +1,11 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { Module } from '@nestjs/common';
 import { MailConfig } from 'src/global/config';
+import { MailService } from './mail.service';
 
 @Module({
   imports: [MailerModule.forRoot(MailConfig)],
-  providers: [],
+  providers: [MailService],
+  exports: [MailService],
 })
 export class MailModule {}
