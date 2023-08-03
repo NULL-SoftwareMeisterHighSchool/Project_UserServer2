7fce1d48d87c7bacf9241cd8369d63c09666d1ba
onee-only <kimww0306@gmail.com>
2023-08-03 16:38:49 +0900

    ADD: send mail request dto cnrk

--- src/domain/auth/dto/request/index.ts
+++ src/domain/auth/dto/request/index.ts
@@ -1,3 +1,4 @@
 export { StudentSignupRequestDto } from './student-signup.request.dto';
 export { LoginRequestDto } from './login.request.dto';
 export { RefreshRequestDto } from './refresh.request.dto';
+export { SendMailRequestDto } from './send-mail.request.dto';
