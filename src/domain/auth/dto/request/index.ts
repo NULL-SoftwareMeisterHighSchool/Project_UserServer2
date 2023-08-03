b154676dab4e70362d4841f49d9064aecf314ca0
onee-only <kimww0306@gmail.com>
2023-08-03 15:45:27 +0900

    ADD: refresh request dto 추가

--- src/domain/auth/dto/request/index.ts
+++ src/domain/auth/dto/request/index.ts
@@ -1,2 +1,3 @@
 export { StudentSignupRequestDto } from './student-signup.request.dto';
 export { LoginRequestDto } from './login.request.dto';
+export { RefreshRequestDto } from './refresh.request.dto';
