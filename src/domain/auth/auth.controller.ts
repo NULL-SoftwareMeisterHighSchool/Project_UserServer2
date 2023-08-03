2e62706b245cef60dd298a6b01903a36b2788842
onee-only <kimww0306@gmail.com>
2023-08-03 15:46:36 +0900

    ADD: logout route 추가

--- src/domain/auth/auth.controller.ts
+++ src/domain/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { LoginRequestDto, StudentSignupRequestDto } from './dto/request';
-import { LoginService, StudentSignupService } from './services';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  LoginRequestDto,
+  RefreshRequestDto,
+  StudentSignupRequestDto,
+} from './dto/request';
+import { LoginService, LogoutService, StudentSignupService } from './services';
 import { LoginResponseDto } from './dto/response';
 
 @Controller('auth')
@@ -8,6 +12,7 @@ export class AuthController {
   constructor(
     private readonly studentSignupService: StudentSignupService,
     private readonly loginService: LoginService,
+    private readonly logoutService: LogoutService,
   ) {}
 
   @Post('signup/student')
@@ -16,7 +21,14 @@ export class AuthController {
   }
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() request: LoginRequestDto): Promise<LoginResponseDto> {
     return await this.loginService.execute(request);
+  }
+
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  async logout(@Body() request: RefreshRequestDto): Promise<void> {
+    return await this.logoutService.execute(request);
   }
 }
