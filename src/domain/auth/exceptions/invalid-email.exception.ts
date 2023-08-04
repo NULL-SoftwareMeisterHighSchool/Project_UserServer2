f436943eaa805c6785daaef984854e40c17b80f9
onee-only <kimww0306@gmail.com>
2023-08-04 16:55:17 +0900

    FIX: invalid email exception 수정

--- src/domain/auth/exceptions/invalid-email.exception.ts
+++ src/domain/auth/exceptions/invalid-email.exception.ts
@@ -3,6 +3,6 @@ import { EMAIL_SUFFIXES } from '../constants/email-suffixes.constant';
 
 export class InvalidEmailException extends BadRequestException {
   constructor() {
-    super(`이메일은 [${EMAIL_SUFFIXES}] 만 사용할 수 있습니다`);
+    super(`이메일은 [${Object.values(EMAIL_SUFFIXES)}] 만 사용할 수 있습니다`);
   }
 }
