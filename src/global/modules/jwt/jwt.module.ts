672732c8cf70f54e40c406e389399897b3289f15
onee-only <kimww0306@gmail.com>
2023-08-04 16:54:34 +0900

    ADD: jwt module에 global option 추가

--- src/global/modules/jwt/jwt.module.ts
+++ src/global/modules/jwt/jwt.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt';
 import { JwtModuleConfig } from 'src/global/config';
 
+@Global()
 @Module({
   imports: [
     NestJwtModule.registerAsync({ useFactory: () => JwtModuleConfig() }),
