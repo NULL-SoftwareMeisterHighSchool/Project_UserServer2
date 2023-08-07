98b42f77d3c990d9cb54521623e41d8b6cb3c3f0
onee-only <kimww0306@gmail.com>
2023-08-07 22:24:40 +0900

    ADD: get user & get me route 추가

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,15 +1,36 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { GetUser } from 'src/global/decorators/get-user-info.decorator';
 import { AuthGuard } from 'src/global/guards/auth.guard';
 import { UserInfo } from 'src/global/types/user-info.type';
+import { GetUserResponseDto } from './dto/response';
+import { GetUserService } from './services';
 
 @Controller('users')
 export class UserController {
-  constructor() {}
+  constructor(private readonly getUserService: GetUserService) {}
+
+  @Get(':userID')
+  async getUser(
+    @Param('userID', ParseIntPipe) userID: number,
+  ): Promise<GetUserResponseDto> {
+    return this.getUserService.execute(userID);
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard)
+  async getMe(@GetUser() userInfo: UserInfo): Promise<GetUserResponseDto> {
+    return this.getUserService.execute(userInfo.id);
+  }
 
   @Get('me/tiny')
   @UseGuards(AuthGuard)
-  getMe(@GetUser() userInfo: UserInfo): UserInfo {
+  getMeTiny(@GetUser() userInfo: UserInfo): UserInfo {
     return userInfo;
   }
 }
