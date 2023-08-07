1ce99268b96288c721eafc18e7323a70b1d18183
onee-only <kimww0306@gmail.com>
2023-08-07 23:50:07 +0900

    update me route 추가

--- src/domain/user/user.controller.ts
+++ src/domain/user/user.controller.ts
@@ -1,24 +1,39 @@
 import {
+  Body,
   Controller,
   Get,
   Param,
   ParseIntPipe,
+  Put,
   UseGuards,
 } from '@nestjs/common';
 import { GetUser } from 'src/global/decorators/get-user-info.decorator';
 import { AuthGuard } from 'src/global/guards/auth.guard';
 import { UserInfo } from 'src/global/types/user-info.type';
 import { GetUserResponseDto } from './dto/response';
-import { GetUserService } from './services';
+import { GetUserService, UpdateUserService } from './services';
+import { UpdateMeRequestDto } from './dto/request';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly getUserService: GetUserService) {}
+  constructor(
+    private readonly getUserService: GetUserService,
+    private readonly updateUserService: UpdateUserService,
+  ) {}
 
   @Get('me')
   @UseGuards(AuthGuard)
   async getMe(@GetUser() userInfo: UserInfo): Promise<GetUserResponseDto> {
     return this.getUserService.execute(userInfo.id);
+  }
+
+  @Put('me')
+  @UseGuards(AuthGuard)
+  async updateMe(
+    @GetUser() userInfo: UserInfo,
+    @Body() request: UpdateMeRequestDto,
+  ): Promise<void> {
+    return await this.updateUserService.execute(userInfo.id, request);
   }
 
   @Get(':userID')
