e0f18a1fba4b9ffde3bce00585e5ee3ac23ded6f
onee-only <kimww0306@gmail.com>
2023-08-08 00:35:42 +0900

    UPDATE: bad request -> forbidden

--- src/domain/user/exceptions/incorrect-password.exception.ts
+++ src/domain/user/exceptions/incorrect-password.exception.ts
@@ -1,6 +1,6 @@
-import { BadRequestException } from '@nestjs/common';
+import { ForbiddenException } from '@nestjs/common';
 
-export class IncorrectPasswordException extends BadRequestException {
+export class IncorrectPasswordException extends ForbiddenException {
   constructor() {
     super('현재 비밀번호가 틀렸습니다');
   }
