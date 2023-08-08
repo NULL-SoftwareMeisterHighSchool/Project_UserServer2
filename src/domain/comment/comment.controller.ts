1114400e630cb91fc633aeaaa9499b55814b9751
onee-only <kimww0306@gmail.com>
2023-08-09 02:12:43 +0900

    ADD: create comment route 추가

--- src/domain/comment/comment.controller.ts
+++ src/domain/comment/comment.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { GetUser } from 'src/global/decorators/get-user-info.decorator';
+import { AuthGuard } from 'src/global/guards/auth.guard';
+import { UserInfo } from 'src/global/types/user-info.type';
+import { CreateCommentRequestDto } from './dto/request';
+import { CreateCommentService } from './services';
 
-@Controller('comments')
-export class CommentController {}
+@Controller('articles/:articleID/comments')
+export class CommentController {
+  constructor(private readonly createCommentService: CreateCommentService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async createComment(
+    @GetUser() userInfo: UserInfo,
+    @Param('articleID', ParseIntPipe) articleID: number,
+    @Body() request: CreateCommentRequestDto,
+  ): Promise<void> {
+    return await this.createCommentService.execute(
+      userInfo,
+      articleID,
+      request,
+    );
+  }
+}
