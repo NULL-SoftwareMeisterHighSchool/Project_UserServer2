3e14692c1535f0071aac47b84e70f8fefc54211b
onee-only <kimww0306@gmail.com>
2023-08-08 23:50:42 +0900

    ADD: create article route 추가

--- src/domain/article/article.controller.ts
+++ src/domain/article/article.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/global/decorators/get-user-info.decorator';
+import { AuthGuard } from 'src/global/guards/auth.guard';
+import { UserInfo } from 'src/global/types/user-info.type';
+import { CreateArticleRequestDto } from './dto/request';
+import { CreateArticleResponseDto } from './dto/response';
+import { CreateArticleService } from './services';
+import { RPCExceptionFilter } from 'src/global/exceptions/filters/rpc-exception.filter';
 
 @Controller('articles')
-export class ArticleController {}
+@UseFilters(RPCExceptionFilter)
+export class ArticleController {
+  constructor(private readonly createArticleService: CreateArticleService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async createArticle(
+    @GetUser() userInfo: UserInfo,
+    @Body() request: CreateArticleRequestDto,
+  ): Promise<CreateArticleResponseDto> {
+    return await this.createArticleService.execute(userInfo, request);
+  }
+}
