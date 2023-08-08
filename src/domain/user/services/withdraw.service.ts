0d2893010c053addbbd27a1be9c1be541504b99d
onee-only <kimww0306@gmail.com>
2023-08-08 10:16:21 +0900

    FIX: withdraw service 수정

--- src/domain/user/services/withdraw.service.ts
+++ src/domain/user/services/withdraw.service.ts
@@ -1,21 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { UserRepository } from '../repositories';
+import { Injectable, Logger } from '@nestjs/common';
+import { StatRepository, UserRepository } from '../repositories';
 import { UserNotFoundException } from '../exceptions';
 
 @Injectable()
 export class WithdrawService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly statRepository: StatRepository,
+  ) {}
 
   async execute(userID: number): Promise<void> {
     const user = await this.userRepository
-      .findOneOrFail({
-        where: { id: userID },
-        relations: { stacks: true },
-      })
-      .catch(() => {
+      .findOneByOrFail({ id: userID })
+      .catch((e) => {
+        new Logger().error(e);
         throw new UserNotFoundException(userID);
       });
 
+    await this.statRepository.delete({ id: userID });
     await this.userRepository.delete(user);
   }
 }
