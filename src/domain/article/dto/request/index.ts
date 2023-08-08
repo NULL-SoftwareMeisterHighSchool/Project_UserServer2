4c480d13c238a23a5ea5383e139dae64d4a2e063
onee-only <kimww0306@gmail.com>
2023-08-09 01:03:17 +0900

    ADD: set visibility request dto 추가

--- src/domain/article/dto/request/index.ts
+++ src/domain/article/dto/request/index.ts
@@ -1,2 +1,3 @@
 export { CreateArticleRequestDto } from './create-article.request.dto';
 export { UpdateArticleRequestDto } from './update-article.request.dto';
+export { SetVisibilityReqeustDto } from './set-visibility.request.dto';
