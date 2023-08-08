d11314064cbd742d8f7c3daf039799229a86398c
onee-only <kimww0306@gmail.com>
2023-08-08 14:34:30 +0900

    FIX: bssm email suffix 수정

--- src/domain/auth/constants/email-suffixes.constant.ts
+++ src/domain/auth/constants/email-suffixes.constant.ts
@@ -3,6 +3,6 @@ import { SchoolType } from 'src/domain/user/enums';
 export const EMAIL_SUFFIXES: Record<SchoolType, string> = {
   GSM: 'gsm.hs.kr',
   DSM: 'dsm.hs.kr',
-  BSM: 'bsm.hs.kr',
+  BSM: 'bssm.hs.kr',
   DGSM: 'dgsm.hs.kr',
 };
