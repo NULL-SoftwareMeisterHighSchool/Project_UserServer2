deab799f16c90d72fcfba1c72d6bec1cd76814de
onee-only <kimww0306@gmail.com>
2023-08-09 00:38:32 +0900

    ADD: get article route 추가

--- src/domain/article/article.controller.ts
+++ src/domain/article/article.controller.ts
@@ -1,16 +1,32 @@
-import { Body, Controller, Post, UseFilters, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { GetUser } from 'src/global/decorators/get-user-info.decorator';
 import { AuthGuard } from 'src/global/guards/auth.guard';
 import { UserInfo } from 'src/global/types/user-info.type';
 import { CreateArticleRequestDto } from './dto/request';
-import { CreateArticleResponseDto } from './dto/response';
-import { CreateArticleService } from './services';
+import {
+  CreateArticleResponseDto,
+  GetArticleResponseDto,
+} from './dto/response';
+import { CreateArticleService, GetArticleService } from './services';
 import { RPCExceptionFilter } from 'src/global/exceptions/filters/rpc-exception.filter';
+import { OptionalGuard } from 'src/global/guards/optional.guard';
 
 @Controller('articles')
 @UseFilters(RPCExceptionFilter)
 export class ArticleController {
-  constructor(private readonly createArticleService: CreateArticleService) {}
+  constructor(
+    private readonly createArticleService: CreateArticleService,
+    private readonly getArticleService: GetArticleService,
+  ) {}
 
   @Post()
   @UseGuards(AuthGuard)
@@ -19,5 +35,14 @@ export class ArticleController {
     @Body() request: CreateArticleRequestDto,
   ): Promise<CreateArticleResponseDto> {
     return await this.createArticleService.execute(userInfo, request);
+  }
+
+  @Get(':id')
+  @UseGuards(OptionalGuard)
+  async getArticle(
+    @GetUser() userInfo: UserInfo,
+    @Param('id', ParseIntPipe) articleID: number,
+  ): Promise<GetArticleResponseDto> {
+    return await this.getArticleService.execute(userInfo, articleID);
   }
 }
