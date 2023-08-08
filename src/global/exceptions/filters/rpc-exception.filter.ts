55f96f194a243a952958855de89b5e03847b4281
onee-only <kimww0306@gmail.com>
2023-08-08 23:48:06 +0900

    ADD: RPC exception filter 추가

---
+++ src/global/exceptions/filters/rpc-exception.filter.ts
@@ -0,0 +1,29 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ForbiddenException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { ServiceError } from '@grpc/grpc-js';
+import { Status } from '@grpc/grpc-js/build/src/constants';
+
+@Catch()
+export class RPCExceptionFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    if (exception as ServiceError) {
+      const e = exception as ServiceError;
+      switch (e.code) {
+        case Status.UNKNOWN:
+          exception = new InternalServerErrorException(e.message);
+          break;
+        case Status.NOT_FOUND:
+          exception = new NotFoundException(e.message);
+        case Status.PERMISSION_DENIED:
+          exception = new ForbiddenException(e.message);
+      }
+    }
+    super.catch(exception, host);
+  }
+}
