960ca03063c36cc7934cce2d8d7f9577065e7aff
onee-only <kimww0306@gmail.com>
2023-08-08 17:50:40 +0900

    get rank service cnrk

--- src/domain/user/services/index.ts
+++ src/domain/user/services/index.ts
@@ -3,3 +3,4 @@ export { UpdateUserService } from './update-user.service';
 export { GetMyStatService } from './get-my-stat.service';
 export { UpdatePasswordService } from './update-password.service';
 export { WithdrawService } from './withdraw.service';
+export { GetRankService } from './get-rank.service';
