1e3cc43e2aaabcf74325588459349f5dfb7bad43
onee-only <kimww0306@gmail.com>
2023-08-08 17:50:15 +0900

    ADD: get rank response dto 추가

--- src/domain/user/dto/response/index.ts
+++ src/domain/user/dto/response/index.ts
@@ -1,2 +1,3 @@
 export { GetUserResponseDto } from './get-user.response.dto';
 export { GetMyStatResponseDto } from './get-my-stat.response.dto';
+export { GetRankResponseDto } from './get-rank.response.dto';
