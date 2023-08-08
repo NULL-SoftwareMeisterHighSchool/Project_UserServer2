dd5234994d0e3e4eb5f802d001caa2aea5d08eb7
onee-only <kimww0306@gmail.com>
2023-08-09 00:49:31 +0900

    ADD: update article route 추가

--- src/domain/article/article.controller.ts
+++ src/domain/article/article.controller.ts
@@ -5,18 +5,26 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  Put,
   UseFilters,
   UseGuards,
 } from '@nestjs/common';
 import { GetUser } from 'src/global/decorators/get-user-info.decorator';
 import { AuthGuard } from 'src/global/guards/auth.guard';
 import { UserInfo } from 'src/global/types/user-info.type';
-import { CreateArticleRequestDto } from './dto/request';
+import {
+  CreateArticleRequestDto,
+  UpdateArticleRequestDto,
+} from './dto/request';
 import {
   CreateArticleResponseDto,
   GetArticleResponseDto,
 } from './dto/response';
-import { CreateArticleService, GetArticleService } from './services';
+import {
+  CreateArticleService,
+  GetArticleService,
+  UpdateArticleService,
+} from './services';
 import { RPCExceptionFilter } from 'src/global/exceptions/filters/rpc-exception.filter';
 import { OptionalGuard } from 'src/global/guards/optional.guard';
 
@@ -26,6 +34,7 @@ export class ArticleController {
   constructor(
     private readonly createArticleService: CreateArticleService,
     private readonly getArticleService: GetArticleService,
+    private readonly updateArticleService: UpdateArticleService,
   ) {}
 
   @Post()
@@ -44,5 +53,19 @@ export class ArticleController {
     @Param('id', ParseIntPipe) articleID: number,
   ): Promise<GetArticleResponseDto> {
     return await this.getArticleService.execute(userInfo, articleID);
+  }
+
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  async updateArticle(
+    @GetUser() userInfo: UserInfo,
+    @Param('id', ParseIntPipe) articleID: number,
+    @Body() reqeust: UpdateArticleRequestDto,
+  ): Promise<void> {
+    return await this.updateArticleService.execute(
+      userInfo,
+      articleID,
+      reqeust,
+    );
   }
 }
