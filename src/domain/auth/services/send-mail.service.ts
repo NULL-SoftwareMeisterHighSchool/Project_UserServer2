c099cad3d6bc97defaf905cda1e215e513a5136c
onee-only <kimww0306@gmail.com>
2023-08-09 22:43:03 +0900

    ADD: email 발송 시 suffix 검증

--- src/domain/auth/services/send-mail.service.ts
+++ src/domain/auth/services/send-mail.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { MailVerificationManager } from '../utils';
 import { SendMailRequestDto } from '../dto/request';
+import { EMAIL_SUFFIXES } from '../constants';
+import { InvalidEmailException } from '../exceptions';
 
 @Injectable()
 export class SendMailService {
@@ -11,6 +13,17 @@ export class SendMailService {
   async execute(request: SendMailRequestDto): Promise<void> {
     const { email } = request;
 
+    if (this.isEmailInvalid(email)) {
+      throw new InvalidEmailException();
+    }
+
     await this.mailVerificationManager.sendVerification(email);
+  }
+
+  private isEmailInvalid(email: string): boolean {
+    const [, suffix] = email.split('@');
+
+    const includes = Object.values(EMAIL_SUFFIXES).includes(suffix);
+    return !includes;
   }
 }
